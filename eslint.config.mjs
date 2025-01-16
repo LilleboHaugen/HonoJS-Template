2725b3167ffa0427b042754bd601d5af0f4c9de1
Jonas <jonas.lillebo.haugen@politiet.no>
2025-01-16 23:32:35 +0100

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "error", // ✅ This only works if TypeScript parser is used
+    },
+  },
+  eslintConfigPrettier,
+);
